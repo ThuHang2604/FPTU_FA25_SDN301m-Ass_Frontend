d5c6a6f5e3719b8f282abf10b17b695ed023a81d
ThuHang2604 <dothuhang2604@gmail.com>
2025-12-19 01:55:54 +0700

    Update API URL for frontend

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,9 @@ import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css' // Import Bootstrap CSS
 import { Provider } from 'react-redux'
 import store from './redux/store'
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://fptu-fa-25-sdn-301m-assignment.vercel.app'; // Sử dụng proxy từ Vite config
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
